faa8c0d7948c1a8486bcf3a411e07a8dc9f28e4b
ryzxzn <93249104+ryzxzn@users.noreply.github.com>
2021-10-27 16:47:22 +0700

    Update channelplay.py

--- GeezProject/modules/channelplay.py
+++ GeezProject/modules/channelplay.py
@@ -470,7 +470,7 @@ async def play(_, message: Message):
         )
         file_name = get_file_name(audio)
         title = file_name
-        thumb_name = "https://telegra.ph/file/f6086f8909fbfeb0844f2.png"
+        thumb_name = "https://telegra.ph/file/645725c80af9b5a512cc7.jpg"
         thumbnail = thumb_name
         duration = round(audio.duration / 60)
         views = "Locally added"
@@ -814,7 +814,7 @@ async def jiosaavn(client: Client, message_: Message):
         sname = songs.result[0].song
         slink = songs.result[0].media_url
         ssingers = songs.result[0].singers
-        sthumb = "https://telegra.ph/file/f6086f8909fbfeb0844f2.png"
+        sthumb = "https://telegra.ph/file/645725c80af9b5a512cc7.jpg"
         sduration = int(songs.result[0].duration)
     except Exception as e:
         await res.edit("Found Literally Nothing!, You Should Work On Your English.")
